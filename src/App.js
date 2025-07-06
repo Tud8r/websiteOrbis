710408b0b0c960276f449aebce00960ec6803122
Tud8r <tudorceclan28@gmail.com>
2025-07-06 12:51:45 +0300

    added everything

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
 import logo from './logo.svg';
+import phoneImage from './assets/orbisphone.png';
+import { useState } from 'react';
 import './App.css';
 
+
+
+
+
 function App() {
+  const [count, setCount] = useState(0)
+  function handleClick() {
+    setCount(count + 1);
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
-    </div>
+      <main >
+        <div className="hero">
+          <div className='hero-wrapper'>
+            <div className='hero-text-wrapper'>
+              <div className='text-container'>
+                <h1 className='header-text'>Know Your Impact. <br />Change It.</h1>
+                <text className='hero-text'>Orbis helps you track your daily carbon footprint and turn awareness into real, measurable action — for a healthier planet and a better future.</text>
+                <button className='preregister-button'>Pre-Register</button>
+              </div>
+            </div>
+            <div className='hero-image'>
+              <img className='image' src={phoneImage}></img>
+            </div>
+          </div>
+
+        </div>
+
+      </main >
+      {/* <footer className='footer-section'> 
+      </footer>*/}
+    </div >
   );
 }
 
