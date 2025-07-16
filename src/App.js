cf89408964007293c9470b0cdea96e18a9f5495b
Tud8r <tudorceclan28@gmail.com>
2025-07-16 17:11:53 +0300

    Save local changes before merge

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import logo from './logo.svg';
-import phoneImage from './assets/orbisphone.png';
+
 import { useState } from 'react';
 import './App.css';
-
+import Hero from './Components/hero';
 
 
 
@@ -18,21 +18,7 @@ function App() {
 
       </header>
       <main >
-        <div className="hero">
-          <div className='hero-wrapper'>
-            <div className='hero-text-wrapper'>
-              <div className='text-container'>
-                <h1 className='header-text'>Know Your Impact. <br />Change It.</h1>
-                <text className='hero-text'>Orbis helps you track your daily carbon footprint and turn awareness into real, measurable action — for a healthier planet and a better future.</text>
-                <button className='preregister-button'>Pre-Register</button>
-              </div>
-            </div>
-            <div className='hero-image'>
-              <img className='image' src={phoneImage}></img>
-            </div>
-          </div>
-
-        </div>
+        <Hero />
 
       </main >
       {/* <footer className='footer-section'> 
