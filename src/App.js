aa327eab3f1cdf067787927d9f008408902d42f7
Tud8r <tudorceclan28@gmail.com>
2025-07-16 17:01:56 +0300

    added hero

--- src/App.js
+++ src/App.js
@@ -1,39 +1,19 @@
 import logo from './logo.svg';
-import phoneImage from './assets/orbisphone.png';
 import { useState } from 'react';
 import './App.css';
-
-
+import Hero from './components/hero.js';
+import Features from './components/features.js';
 
 
 
 function App() {
-  const [count, setCount] = useState(0)
-  function handleClick() {
-    setCount(count + 1);
-  }
   return (
     <div className="App">
       <header className="App-header">
-
       </header>
       <main >
-        <div className="hero">
-          <div className='hero-wrapper'>
-            <div className='hero-text-wrapper'>
-              <div className='text-container'>
-                <h1 className='header-text'>Know Your Impact. <br />Change It.</h1>
-                <text className='hero-text'>Orbis helps you track your daily carbon footprint and turn awareness into real, measurable action — for a healthier planet and a better future.</text>
-                <button className='preregister-button'>Pre-Register</button>
-              </div>
-            </div>
-            <div className='hero-image'>
-              <img className='image' src={phoneImage}></img>
-            </div>
-          </div>
-
-        </div>
-
+        <Hero />
+        <Features />
       </main >
       {/* <footer className='footer-section'> 
       </footer>*/}
